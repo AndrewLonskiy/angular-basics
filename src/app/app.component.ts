2184cb9b04a10a22d177a41548988efd96033f6a
Evenstar <evenstar@MacBook-Pro-Evenstar-2.local>
2020-01-08 16:05:04 +0200

    angular components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+
+
+
+
+
+  // now: Date = new Date()
+
+// toggle = false
+//
+//   arr = [1,1,3,5,7,8,9]
+//
+//   objs = [
+//     {title: 'Post 1', author: 'Vlad', comments:[
+//         {name: 'Max', text: 'Lorem 1'},
+//         {name: 'Max', text: 'Lorem 2'},
+//         {name: 'Max', text: 'Lorem 3'}
+//       ]},
+//     {title: 'Post 2', author: 'Vlad 2', comments:[
+//         {name: 'Max 2', text: 'Lorem 1'},
+//         {name: 'Max 2', text: 'Lorem 2'},
+//         {name: 'Max 2', text: 'Lorem 3'}
+//       ]}
+//   ]
+
+
+
+
+  // title = 'Some text'
+  //
+  // onInput(event: any){
+  //   this.title = event.target.value
+  // }
+
+
+
+
+  // title = 'angular-basics';
+  // number = 42
+  // arr = [1, 2, 3]
+  //
+  // obj = {a: 1, b: {c: 2}}
+  //
+  // inputValue = ''
+  //
+  // // img = 'https://reactjsexample.com/content/images/2016/08/20160828182925.jpg'
+  //
+  // constructor() {
+  //   // setTimeout(() => {
+  //   //   console.log('Timeout is over')
+  //   //   this.img = 'https://res.cloudinary.com/teepublic/image/private/s--eF-l8J9P--/t_Preview/b_rgb:ffffff,c_limit,f_jpg,h_630,q_90,w_630/v1539378444/production/designs/3308909_0.jpg'
+  //   // }, 5000)
+  //
+  // }
+  //
+  // onInput(event?: KeyboardEvent) {
+  //   this.inputValue = (<HTMLInputElement>event.target).value
+  // }
+  // onBlur(str: string ) {
+  //   this.inputValue = str
+  // }
+  //
+  // onClick() {
+  //   console.log('Click')
+  // }
+
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -3,7 +3,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-post',
-  templateUrl: './post.component.html'
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss']
 })
 
 export class PostComponent {
